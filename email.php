61f27e579e0c982fdbc2fa8dd5a887c6b5af4d37
John Michael Gadot <jmgadot.dev@gmail.com>
2022-10-13 10:24:33 +0800

    Add files via upload

---
+++ email.php
@@ -0,0 +1,35 @@
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "no_reply@gsc.edu.ph";
+$mail->Password   = "ICTOconn3ct22";
+
+$mail->IsHTML(true);
+$mail->AddAddress("jmgadot.official@gmail.com", "Mike");
+$mail->SetFrom("no_reply@gsc.edu.ph", "ICTO SIS");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
+
+?>
